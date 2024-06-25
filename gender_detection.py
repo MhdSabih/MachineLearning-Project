f9f31777be6b5b7bc1e3f2b2d32f9164303190f9
Affan M. Ghazali <110742234+MhdSabih@users.noreply.github.com>
2024-06-26 04:37:59 +0500

    updated gender_dedection.py

--- gender_detection.py
+++ gender_detection.py
@@ -2,6 +2,7 @@ import cv2
 import os
 from deepface import DeepFace
 
+
 image_files = [
     "sabih.jpg",
     "ahmed.jpg",
@@ -12,3 +13,38 @@ image_files = [
     "woman1.jpg",
 ]
 
+
+for image_file in image_files:
+    image_path = os.path.join(os.getcwd(), image_file)
+
+    frame = cv2.imread(image_path)
+
+    if frame is None:
+        print(f"Could not read image {image_file}")
+        continue
+
+    results = DeepFace.analyze(frame, actions=["gender"], enforce_detection=False)
+
+    for result in results:
+        x, y, w, h = (
+            result["region"]["x"],
+            result["region"]["y"],
+            result["region"]["w"],
+            result["region"]["h"],
+        )
+        dominant_gender = result["dominant_gender"]
+
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+        cv2.putText(
+            frame,
+            dominant_gender,
+            (x, y - 10),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.9,
+            (36, 255, 12),
+            2,
+        )
+
+
+cv2.destroyAllWindows()
