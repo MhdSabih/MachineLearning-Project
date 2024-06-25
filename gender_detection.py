c8b943a4677b7374f54b0b137a956ef75e4d710b
Affan M. Ghazali <110742234+MhdSabih@users.noreply.github.com>
2024-06-26 04:24:43 +0500

    created gender_detection.py

---
+++ gender_detection.py
@@ -0,0 +1,14 @@
+import cv2
+import os
+from deepface import DeepFace
+
+image_files = [
+    "sabih.jpg",
+    "ahmed.jpg",
+    "abd.jpg",
+    "sufyan.jpg",
+    "cat.jpg",
+    "dure.jpg",
+    "woman1.jpg",
+]
+
